ed6b7494a4e3804f49caccc4cb56beefb89dcf88
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-03 23:53:52 +0530

    Test 1

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-import os from 'os';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-console.log(os.platform());
-console.log(os.version());
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// const os = require('os');
+// import os from 'os';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
 // console.log(os.platform());
 // console.log(os.version());
-// console.log(__dirname);
-// console.log(__filename);
 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // const os = require('os');
+
+// // console.log(os.platform());
+// // console.log(os.version());
+// // console.log(__dirname);
+// // console.log(__filename);
+
+console.log("Node");

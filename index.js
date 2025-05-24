813bdfeabe651e47178b6e8b358445b729ce0766
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-24 16:08:09 +0530

    Creating Basic APIs

--- index.js
+++
@@ -1,8 +0,0 @@
-var {sum, divide} = require("./calculate");
-console.log(cal);
-
-var totalSumValue = sum(2, 6);
-console.log(totalSumValue);
-
-var totalDevideValue = divide(6, 3);
-console.log(totalDevideValue);
---
+++ services/product-service.js
@@ -0,0 +1,11 @@
+function getAllProduct() {
+  let product = [];
+
+  product.push({
+    name: "Apple",
+    category: "Fruit",
+  });
+  return product;
+}
+
+module.exports = { getAllProduct };

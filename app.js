fbc7c2c5d772117cece32637480d9031fbecb4ba
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-26 17:01:44 +0530

    Move To Service Folder Separate

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const os = require("os");
 const http = require("http");
 const PORT = 3000;
+const productService = require("./services/product-service");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/plain" });
-  res.end(
-    JSON.stringify({
-      name: "Apple",
-      color: "Red",
-    })
-  );
+  if (req.method === "POST" && req.url == "/create-product") {
+    res.writeHead(200, { "Content-type": "application/JSON" });
+    res.end(
+      JSON.stringify({
+        name: "Apple",
+        color: "Red",
+      })
+    );
+  }
+
+  if (req.method === "GET" && req.url == "/get-product") {
+    res.writeHead(200, { "Content-type": "application/JSON" });
+    res.end(JSON.stringify(productService.getProducts()));
+  }
 });
 
 server.listen(PORT, () => {
---
+++ services/product-service.js
@@ -0,0 +1,15 @@
+function getProducts() {
+  let product = [];
+
+  product.push({
+    name: "Orange",
+    price: 200.0,
+  });
+
+  product.push({
+    name: "Orange",
+    price: 200.0,
+  });
+}
+
+module.exports = { getProducts };
